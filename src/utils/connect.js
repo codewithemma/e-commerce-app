8bc2a3f731f5478475dc9a80ff7f02e090cc61f6
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-05-31 14:43:59 +0100

    login test

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 const uri = process.env.MONGO_URI;
-let isConnected = false;
 const localUrl = "mongodb://localhost:27017/exclusive_db";
 const url = process.env.NODE_ENV === "production" ? uri : localUrl;
+let isConnected = false;
 export async function connectDB() {
   if (isConnected) {
     console.log("connected to db....");
     return;
   }
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     isConnected = true;
     console.log("MongoDB Connected");
   } catch (error) {
