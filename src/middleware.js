02d1962bb8c486ca646dce40710e2b082f688224
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-06-12 03:06:33 +0100

    ✔️

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth({
   callbacks: {
     authorized: ({ req, token }) => {
-      if (req.nextUrl.pathname === "/auth/admin") {
+      if (req.nextUrl.pathname === "/protected/admin") {
         return token?.role === "superadmin" || token?.role === "admin";
       }
       return Boolean(token);
