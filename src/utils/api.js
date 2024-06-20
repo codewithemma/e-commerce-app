9633922ab1afed798dc024b783d826c7ada2abf9
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-06-20 13:08:57 +0100

    minor bug fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,3 @@
-const uri = "https://exclusivee-commerce-app.vercel.app/";
+const uri = "https://exclusivee-commerce-app.vercel.app";
 const localUrl = "http://localhost:3000";
 export const url = process.env.NODE_ENV === "production" ? uri : localUrl;
