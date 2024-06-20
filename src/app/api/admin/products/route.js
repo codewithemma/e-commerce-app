d6626d0120297a2dc46cc5db7c095d4e9ebc67cc
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-06-20 13:48:27 +0100

    security fix

--- src/app/api/admin/products/route.js
+++ src/app/api/admin/products/route.js
@@ -1,26 +1,8 @@
 import Product from "@/models/Product";
 import { connectDB } from "@/utils/connect";
 import { StatusCodes } from "http-status-codes";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-
-// export const GET = async (req, res) => {
-//   try {
-//     await connectDB();
-//     const products = await Product.find().sort({
-//       _id: -1,
-//     });
-//     return new NextResponse(
-//       JSON.stringify(products, { status: StatusCodes.OK })
-//     );
-//   } catch (error) {
-//     return new NextResponse(
-//       JSON.stringify(
-//         { message: "Something went wrong" },
-//         { status: StatusCodes.BAD_REQUEST }
-//       )
-//     );
-//   }
-// };
 
 export const POST = async (req) => {
   //SESSION VALIDATION
