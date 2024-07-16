ab9b2ae0e6e28a7591c3484f4cb3444f27dacbba
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-07-16 12:02:19 +0100

    test

--- src/app/user/admin/create-item/page.jsx
+++ src/app/user/admin/create-item/page.jsx
@@ -1,7 +1,9 @@
 import Product from "@/models/Product";
 import CreateItem from "./productForm/ProductForm";
+import { connectDB } from "@/utils/connect";
 
 const page = async () => {
+  await connectDB();
   const products = await Product.find({});
   return <CreateItem productData={JSON.parse(JSON.stringify(products))} />;
 };
